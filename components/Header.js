f00896c76742151d66bdc4fd0aed342fdc48a85a
avijitsaha2997 <jit.avi.saha2997@gmail.com>
2023-01-09 22:01:13 +0600

    second commit

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,56 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
-import { SearchIcon, MenuIcon, UserCircleIcon, UserIcon, GlobeAltIcon } from '@heroicons/react/solid'
+import { SearchIcon, MenuIcon, UserCircleIcon, UsersIcon, GlobeAltIcon } from '@heroicons/react/solid'
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
+import { useRouter } from "next/router";
 
 
 
+function Header({ placeholder }) {
+    const [searchInput, setSearchInput] = useState("");
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+    const [noOfGuests, setNoOfGuests] = useState(1);
+    const router = useRouter();
 
-function Header() {
+
+    const handleSelect = (ranges) => {
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate)
+    }
+
+    const resetInput = () => {
+        setSearchInput("");
+    }
+
+    const search = () => {
+        setSearchInput("");
+        router.push({
+            pathname: "/search",
+            query: {
+                location: searchInput,
+                startDate: startDate.toISOString(),
+                endDate: endDate.toISOString(),
+                noOfGuests
+            }
+        })
+    }
+
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: "selection"
+
+    }
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
             {/*left*/}
-            <div className="relative flex items-center h-10 cursor-pointer my-auto">
+            <div
+                onClick={() => router.push("/")}
+                className="relative flex items-center h-10 cursor-pointer my-auto">
                 <Image src="https://www.pngall.com/wp-content/uploads/2017/05/Map-Marker-Free-Download-PNG.png" alt=""
                     layout="fill"
                     objectFit="contain"
@@ -22,7 +62,10 @@ function Header() {
 
             {/*middle*/}
             <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-md">
-                <input className="flex-grow pl-5 outline-none bg-transparent text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Start your search" />
+                <input
+                    value={searchInput}
+                    onChange={(e) => setSearchInput(e.target.value)}
+                    className="flex-grow pl-5 outline-none bg-transparent text-sm text-gray-600 placeholder-gray-400" type="text" placeholder={placeholder || "Start your search"} />
                 <SearchIcon className="hidden md:inline-flex h-8 bg-orange-300 text-white rounded-full p-2 cursor-pointer md:mx-2" />
             </div>
 
@@ -36,6 +79,34 @@ function Header() {
                     <UserCircleIcon className="h-6 cursor-pointer" />
                 </div>
             </div>
+
+            {searchInput && (
+                <div className="flex flex-col col-span-3 mx-auto">
+                    <DateRangePicker
+                        ranges={[selectionRange]}
+                        minDate={new Date()}
+                        rangeColors={["#d9ac26"]}
+                        onChange={handleSelect}
+                    />
+                    <div className="flex items-center border-b mb-4">
+                        <h2 className="text-2xl flex-grow font-semibold">Number of Members</h2>
+
+                        <UsersIcon className="h-5" />
+
+                        <input value={noOfGuests}
+                            min={1} type="number" className="w-12 pl-2 text-lg outline-none text-yellow-600"
+                            onChange={(e) => setNoOfGuests(e.target.value)}
+                        />
+                    </div>
+                    <div className="flex">
+
+                        <button className="flex-grow text-gay-500" onClick={resetInput}>Cancel</button>
+
+                        <button onClick={search} className="flex-grow text-yellow-600">Search</button>
+
+                    </div>
+                </div>
+            )}
 
         </header>
     );
--- pages/index.js
+++ pages/index.js
@@ -32,9 +32,11 @@ export default function Home({ exploreData, cardsData }) {
 
           {/*pull data from server - api endpoints*/}
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+
             {exploreData && exploreData.map(({ img, distance, location }) => (
               <SmallCard key={img} img={img} location={location} distance={distance} />
             ))}
+            
           </div>
         </section>
 
